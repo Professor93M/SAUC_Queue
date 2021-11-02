b80cca4343a13622b783bf2d46df5f27f9b3e411
watan <drmody007.d0@gmail.com>
2021-11-02 20:34:07 +0300

    oko

--- resources/js/Pages/Index.js
+++ resources/js/Pages/Index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Head } from "@inertiajs/inertia-react";
 
 export default function Welcome(props) {
+    let [counter, setCounter] = useState(1);
+
+    const handleClick = () => {
+        window.print();
+        setCounter((counter = counter + 1));
+    };
     return (
         <>
-            <h1>jjfiejfij</h1>
+            <div className="h-32 w-32">
+                <img src="/Logo.png" alt="Logo" />
+            </div>
+            <h1>جمهورية العراق</h1>
+            <h2>وزارة التعليم العالي والبحث العلمي</h2>
+            <h2>كلية شط العرب الجامعة الاهلية</h2>
+
+            <p>
+                مرحبا بكم في كليتنا يرجى الضغط على الزر ادناه للحصول على رقم
+                تسلسل
+            </p>
+            <h3> {counter} </h3>
+            <button onClick={handleClick}>اضغط هنا</button>
         </>
     );
 }
